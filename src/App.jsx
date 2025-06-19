947213d6d6ea43510451af0a56ae0e760cc88dcd
Samruddhi0857 <samruddhi0857@gmail.com>
2025-06-19 11:06:25 +0530

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import Projects from "./components/Projects/Projects"
 import Education from "./components/Education/Education";
 
 import Footer from "./components/Footer/Footer";
-import BlurBlob from './components/BlurBlob';
+import Purpleflash from './components/Purpleflash';
 
 const App = () => {
   return (
     <div className="bg-[#0d1117]">
 
-       <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+       <Purpleflash position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
 
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] 	bg-[#0d1117]"></div>
       
